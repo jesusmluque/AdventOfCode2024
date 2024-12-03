d7d79695c4d3fd96371c22bcd5bde2642d117b22
Jesus Montero <jesusmluque@gmail.com>
2024-12-03 17:45:21 +0100

    Third Day: Mull It Over -> Refactor foldLeft to flatMap, much cleaner

--- src/main/scala/MullItOver.scala
+++ src/main/scala/MullItOver.scala
@@ -1,16 +1,14 @@
 object MullItOver:
 
   def totalSumAfterParse(rawInstructions: List[String]):Long =
-    rawInstructions.foldLeft(List[Long]()): (acc, next) =>
+    rawInstructions.flatMap: next =>
       "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)".r.findAllMatchIn(next).map(_.subgroups).toList.map: pair =>
         pair.map(_.toLong).product
-      ++ acc
     .sum
 
   def totalSumAfterParseWithDontAndDoIns(rawInstructions: List[String]): Long =
-    val allInstructions = rawInstructions.foldLeft(""): (acc, str) =>
-      acc ++ str
-    val instructionsSeparatedByDonts = allInstructions.split("don't\\(\\)").toList
-    val instructionsFiltered = instructionsSeparatedByDonts.tail.foldLeft(List(instructionsSeparatedByDonts.head)): (acc, partialStr) =>
-      partialStr.split("do\\(\\)").toList.tail ++ acc
+    val allInstructionsString = rawInstructions.foldLeft("")( (acc, str) => acc ++ str)
+    val instructionsSeparatedByDonts = allInstructionsString.split("don't\\(\\)").toList
+    val instructionsFiltered = instructionsSeparatedByDonts.head :: instructionsSeparatedByDonts.tail.flatMap: partialStr =>
+      partialStr.split("do\\(\\)").toList.tail
     totalSumAfterParse(instructionsFiltered)
