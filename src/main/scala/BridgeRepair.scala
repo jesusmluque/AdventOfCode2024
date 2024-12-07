30f4f9fe73b9cfe4baad0923a1e1e5410627e941
Jesus Montero <jesusmluque@gmail.com>
2024-12-07 12:11:10 +0100

    Day 7: Refactor

--- src/main/scala/BridgeRepair.scala
+++ src/main/scala/BridgeRepair.scala
@@ -8,18 +8,14 @@ object BridgeRepair:
         val first = equation.head
         val second = equation.tail.head
         val rest = equation.tail.tail
-        operations.foldLeft(List[Option[Long]]()): (acc, next) =>
-          next match
-            case "+" => solveEquation(target, (first + second) :: rest) :: acc
-            case "*" => solveEquation(target, (first * second) :: rest) :: acc
-            case "||" => solveEquation(target, (first.toString + second.toString).toLong :: rest) :: acc
-        .find(_.isDefined) match
-          case None => None
-          case Some(r) => r
+        operations.iterator.map:
+            case "+" => solveEquation(target, (first + second) :: rest)
+            case "*" => solveEquation(target, (first * second) :: rest)
+            case "||" => solveEquation(target, (first.toString + second.toString).toLong :: rest)
+        .collectFirst {case Some(r) => r}
 
     rawEquations.map: raw =>
-      val eq = raw.split(":")
-      val target = eq(0).toLong
-      val numbers = eq(1).split(" ").filter(_ != "").map(_.toLong).toList
-      solveEquation(target, numbers)
+      val Array(target, n) = raw.split(":")
+      val numbers = n.trim.split("\\s+").map(_.toLong).toList
+      solveEquation(target.toLong, numbers)
     .map(_.getOrElse(0L)).sum
