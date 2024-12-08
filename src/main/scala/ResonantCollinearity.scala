ce97db77acf0df4892819babd146c0391e0afbaa
Jesus Montero <jesusmluque@gmail.com>
2024-12-08 13:53:30 +0100

    Day 8: Refactor

--- src/main/scala/ResonantCollinearity.scala
+++ src/main/scala/ResonantCollinearity.scala
@@ -1,41 +1,32 @@
 object ResonantCollinearity:
 
-  def countAntinodesIn(rawAntennas: List[String]): Int =
-    val antennas = buildAntennasPlaces(rawAntennas)
-    val limits = (rawAntennas.size, rawAntennas.head.length)
-    antennas.foldLeft(Set[(String, (Int,Int))]()): (acc, antenna) =>
-      val sameAntennas = getAllAntennasSameAs(antenna, antennas)
-      sameAntennas.foldLeft(acc): (acc2, ant) =>
-        val diff = (antenna._2._1 - ant._2._1, antenna._2._2 - ant._2._2)
-        List(("#", (antenna._2._1 + diff._1, antenna._2._2 + diff._2)), ("#", (ant._2._1 - diff._1, ant._2._2 - diff._2)))
-          .filter: antiNode =>
-            antiNode._2._1 >= 0 && antiNode._2._1 < limits._1 && antiNode._2._2 >= 0 && antiNode._2._2 < limits._2
-        .toSet ++ acc2
-    .size
+  def countAntinodesWithoutReasoningIn(rawAntennas: List[String]): Int =
+    def generateAntinodeFor(antenna1: (String,(Int,Int)), antenna2: (String,(Int,Int))) =
+      val diff = (antenna1._2._1 - antenna2._2._1, antenna1._2._2 - antenna2._2._2)
+      List(("#", (antenna1._2._1 + diff._1, antenna1._2._2 + diff._2)), ("#", (antenna2._2._1 - diff._1, antenna2._2._2 - diff._2)))
+      .filter: antiNode =>
+        antiNode._2._1 >= 0 && antiNode._2._1 < rawAntennas.size && antiNode._2._2 >= 0 && antiNode._2._2 < rawAntennas.head.length
+      .toSet
+    countAntinodesIn(rawAntennas, generateAntinodeFor)
 
   def countAntinodesWithReasoningIn(rawAntennas: List[String]): Int =
+    def generateAntinodeFor(antenna1: (String, (Int,Int)), antenna2: (String, (Int,Int))) =
+      val limits = (rawAntennas.size, rawAntennas.head.length)
+      val diff = (antenna1._2._1 - antenna2._2._1, antenna1._2._2 - antenna2._2._2)
+      def getRangeOfPositions(a: (String, (Int, Int))) =
+        Range(a._2._1, if diff._1 < 0 then -1 else limits._1, diff._1).zip(Range(a._2._2, if diff._2 < 0 then -1 else limits._2, diff._2))
+        .toList.filter(pos => pos._1 >= 0 && pos._1 < limits._1 && pos._2 >= 0 && pos._2 < limits._2)
+      (getRangeOfPositions(antenna1).map(("#", _)) ++ getRangeOfPositions(antenna2).map(("#", _))).toSet
+    countAntinodesIn(rawAntennas, generateAntinodeFor)
+
+  private def countAntinodesIn(rawAntennas: List[String], generateAntinodes: ((String, (Int, Int)), (String, (Int, Int))) => Set[(String, (Int, Int))]) =
+    def buildAntennasPlaces(rawAntennas: List[String]) =
+      rawAntennas.zipWithIndex.toVector.flatMap: row =>
+        row._1.split("").zipWithIndex.map(place => (place._1, (row._2, place._2))).filter(_._1 != ".")
+    def getAllAntennasSameAs(antenna: (String, (Int, Int)), antennas: Vector[(String, (Int, Int))]) =
+      antennas.filter(a => a._1 == antenna._1 && a._2 != antenna._2)
+
     val antennas = buildAntennasPlaces(rawAntennas)
-    val limits = (rawAntennas.size, rawAntennas.head.length)
     antennas.foldLeft(Set[(String, (Int, Int))]()): (acc, antenna) =>
-      val sameAntennas = getAllAntennasSameAs(antenna, antennas)
-      sameAntennas.foldLeft(acc): (acc2, ant) =>
-        val diff = (antenna._2._1 - ant._2._1, antenna._2._2 - ant._2._2)
-
-        def getRangeOfPositions(a: (String, (Int,Int))) =
-          Range(a._2._1, if diff._1 < 0 then -1 else limits._1, diff._1).zip(Range(a._2._2, if diff._2 < 0 then -1 else limits._2, diff._2))
-            .toList.filter: pos =>
-              pos._1 >= 0 && pos._1 < limits._1 && pos._2 >= 0 && pos._2 < limits._2
-
-        (getRangeOfPositions(antenna).map(("#", _)) ++ getRangeOfPositions(ant).map(("#", _))).toSet ++ acc2
+      getAllAntennasSameAs(antenna, antennas).foldLeft(acc)((acc2, ant) => generateAntinodes(antenna, ant) ++ acc2)
     .size
-
-  private def getAllAntennasSameAs(antenna: (String, (Int,Int)), antennas: Vector[(String, (Int, Int))]) =
-    val sameAntennas = antennas.filter: a =>
-      a._1 == antenna._1 && a._2 != antenna._2
-    sameAntennas
-
-  private def buildAntennasPlaces(rawAntennas: List[String]) =
-    rawAntennas.zipWithIndex.toVector.flatMap: row =>
-      row._1.split("").zipWithIndex.map: place =>
-        (place._1, (row._2, place._2))
-    .filter(_._1 != ".")
--- src/test/scala/ResonantCollinearityTest.scala
+++ src/test/scala/ResonantCollinearityTest.scala
@@ -5,9 +5,9 @@ import scala.io.Source
 class ResonantCollinearityTest extends AnyFlatSpec:
 
   "The total numbers of antinode in the example1 file " should " be 14 " in:
-    assert(ResonantCollinearity.countAntinodesIn(Source.fromResource("ResonantCollinearityExample1.txt").getLines().toList) == 14)
+    assert(ResonantCollinearity.countAntinodesWithoutReasoningIn(Source.fromResource("ResonantCollinearityExample1.txt").getLines().toList) == 14)
   "The total numbers of antinode in the exercise file " should " be 259 " in :
-    assert(ResonantCollinearity.countAntinodesIn(Source.fromResource("ResonantCollinearityExercise.txt").getLines().toList) == 259)
+    assert(ResonantCollinearity.countAntinodesWithoutReasoningIn(Source.fromResource("ResonantCollinearityExercise.txt").getLines().toList) == 259)
   "The total numbers of antinode taking into account reasoning in the example1 file " should " be 34 " in :
     assert(ResonantCollinearity.countAntinodesWithReasoningIn(Source.fromResource("ResonantCollinearityExample1.txt").getLines().toList) == 34)
   "The total numbers of antinode taking into account reasoning in the exercise file " should " be 927 " in :
